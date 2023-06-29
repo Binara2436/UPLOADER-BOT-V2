cd0f7063f0bbfa030d2138f25556cc98ecea1f71
Binara2436 <48156132+Binara2436@users.noreply.github.com>
2023-06-29 10:00:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6299210485:AAEb3tjYjE8DmwqX8ZCQ98kx8XcA3Q9Vals")
     
-    API_ID = int(os.environ.get("API_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", "20219667"))
     
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH", "82b82bcd10c857178cc49e22f5e8d67c" )
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -24,10 +24,10 @@ class Config(object):
     
     PROCESS_MAX_TIMEOUT = 3600
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1929795683"))
 
-    SESSION_NAME = "UploadLinkToFileBot"
+    SESSION_NAME = "spectreurluploaderbot"
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongo.db")
 
     MAX_RESULTS = "50"
